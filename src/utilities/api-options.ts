abb0b23a473c04d0cb829186196fbb8ab700ea18
venkat-devish <venkatsuryakiranv@gmail.com>
2023-03-08 12:29:03 +0530

    update: KEY

--- src/utilities/api-options.ts
+++ src/utilities/api-options.ts
@@ -1,9 +1,9 @@
 export const recommendedOptions = {
     method: 'GET',
     url: 'https://shazam.p.rapidapi.com/songs/list-recommendations',
-    params: { key: '484129036', locale: 'en-US' },
+    params: { key: [KEY], locale: 'en-US' },
     headers: {
-        'X-RapidAPI-Key': '1ad0f4702dmsha27ede3d10d9c16p12fc5djsne8e9b35154f7',
+        'X-RapidAPI-Key': [KEY],
         'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
     }
 };
